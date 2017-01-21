4af0ddbeaeac75af378c58d9aaa774f8e22fa732
sandboats <akhil.alt7@gmail.com>
2017-01-21 20:10:33 +0530

    needed changes

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    func averageIsAbove75(a:Double, b:Double, c:Double) -> String {
+        
+        if ((a+b+c)/3.0 > 75){
+            
+            return "Average is below 75, it should return false."
+        }
+        else {
+            
+            return "Average is above 75, it should return true."
+        }
+        
+    }
     
+    // (password % 3 = 0)
     
+  /*  func passwordCombo(username: String, password: Int) -> (String, String){
+        
+        if (username == "Jerry" || "Elaine" || "Michael"){
+            
+            print("")
+        }
+        
+        
+    }
     
+   */ 
     
-    // Implement your functions here
+
     
     
     
